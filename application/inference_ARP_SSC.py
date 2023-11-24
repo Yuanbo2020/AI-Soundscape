35f11316a45f158375feb0c6ad418404f8a2c866
Andrew Mitchell <mitchellacoustics15@gmail.com>
2023-11-24 20:57:58 +0000

    Add optional Arg flag for cuda to bypass config
    This also involved removing some hardcoded cuda = 1 lines to make sure it's possible to run models on CPU.

--- application/inference_ARP_SSC.py
+++ application/inference_ARP_SSC.py
@@ -37,6 +37,7 @@ def count_parameters(model):
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('-model', type=str, required=True)
+    parser.add_argument('-cuda', type=int, required=False)
     args = parser.parse_args()
 
     model_type = args.model
@@ -56,8 +57,8 @@ def main(argv):
     model_event = torch.load(event_model_path, map_location='cpu')
     model.load_state_dict(model_event['state_dict'])
 
-    cuda = 1
-    if config.cuda:
+    cuda = args.cuda if args.cuda is not None else config.cuda
+    if cuda:
         model.cuda()
 
     generator = DataGenerator(batch_size=64)
